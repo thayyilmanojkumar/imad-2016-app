f17567220a939dec6c0cb044e032de40f96aa34e
Manojkumar.T <thayyilmanojkumar@gmail.com>
2016-10-03 16:14:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,12 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function  moveRight() {
+    marginLeft=marginLeft+10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function() {
-    img.style.marginLeft = '100px';
+    var interval = setInterval(moveRight,100);
     
 };
