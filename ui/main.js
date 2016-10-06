81c082b6d38c751b4f40292d378715df6f8f84b1
Manojkumar.T <thayyilmanojkumar@gmail.com>
2016-10-06 22:10:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,1 @@
 console.log('Loaded!');
-var element = document.getElementById('main-text');
-element.innerHTML = 'New Value';
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function  moveRight() {
-    marginLeft=marginLeft+1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function() {
-    var interval = setInterval(moveRight,50);
-    
-};
