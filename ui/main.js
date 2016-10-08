b3b501eb85862eb123d707e167dd60abcde2bd80
Manojkumar.T <thayyilmanojkumar@gmail.com>
2016-10-08 10:09:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,17 @@
 var btn = document.getElementById('counter');
 
-var counter =0;
 btn.onclick=function (){
-    counter = counter+1;
-    var span = document.getElementById('count');
-    span.innerHTML=counter.toString();
+   var request = new XMLHttpRequest();
+   request.onreadystatechange = function(){
+       if(request.readyState == XMLHttpRequest.DONE) {
+           if(request.status == 200) {
+               var counter= requst.responseText;
+               var span = document.getElementById('count');
+               span.innerHTML=counter.toString();
+           }
+       }
+   };
+   request.open('GET','http://http://thayyilmanojkumar.imad.hasura-app.io/counter',true);
+   request.send(null);
     
 };
