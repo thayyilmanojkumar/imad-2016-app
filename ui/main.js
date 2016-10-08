6626dd28d4cdd600963119b993d94f94be2652bd
Manojkumar.T <thayyilmanojkumar@gmail.com>
2016-10-08 09:12:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,9 +4,9 @@ element.innerHTML = 'Manojkumar.T';
 var img = document.getElementById('madi');
 var marginLeft=0;
 function moveRight(){
-  marginLeft = marginLeft + 5;
+  marginLeft = marginLeft + 3;
   img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function () {
-  var interval = setInterval(moveRight,50);
+  var interval = setInterval(moveRight,30);
 };
